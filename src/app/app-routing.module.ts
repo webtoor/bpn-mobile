81536abea88034a6b1498d8c566215f6b5fdc8fa
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2020-02-05 12:06:26 +0700

    Create Page Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 

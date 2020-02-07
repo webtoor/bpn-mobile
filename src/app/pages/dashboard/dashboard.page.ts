704fadbedd2b623f4936b4d571397af25feed89d
Fityan Ali Munshi <fityanalimunshi@gmail.com>
2020-02-07 18:27:26 +0700

    Validation Report Post

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { ApiService } from '../../service/api.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoaderService } from '../../service/loader.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +11,28 @@ import { ApiService } from '../../service/api.service';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
   userAuth;
   locations;
-  constructor(public menu : MenuController, public apiService : ApiService) {
+  constructor(private formBuilder: FormBuilder, public loading: LoaderService, public menu : MenuController, public apiService : ApiService) {
     this.menu.enable(true);
     const data = JSON.parse(localStorage.getItem('authBPN'));
     this.userAuth = data;
+    this.registerForm = this.formBuilder.group({
+      'dtreport' : [null, Validators.required],
+      'location' : [null, [Validators.required]],
+      'terukur' : [null, Validators.required],
+      'tergambar' : [null, Validators.required],
+      'kkp' : [null, Validators.required],
+      'pengukuran' : [null, Validators.required],
+      'pemetaan' : [null, Validators.required],
+      'pbt' : [null, [Validators.required]],
+      'su' : [null, Validators.required],
+      'pengumuman' : [null, Validators.required],
+      'pengesahan' : [null, Validators.required],
+      'keterangan' : [null, Validators.required],
+    });
    }
 
   ngOnInit() {
@@ -23,7 +42,7 @@ export class DashboardPage implements OnInit {
   getLocation(){
     this.apiService.getDataAuth('get-location', this.userAuth['access_token']).subscribe(res => {
      console.log(res)
-      if(res['status'] == 1){
+      if(res['status'] == "1"){
         this.locations = res['data']
       
         //console.log(this.kotakab)
